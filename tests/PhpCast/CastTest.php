a79ce712675c1628bf20a1b24d507a1dce127745
sji <shinji.igarashi@gmail.com>
2020-07-06 14:18:19 +0900

    add test cases

--- tests/PhpCast/CastTest.php
+++ tests/PhpCast/CastTest.php
@@ -21,10 +21,103 @@ class CastTest extends TestCase
 {
     public function testToInt()
     {
+        $this->assertSame(0, Cast::toInt(0));
+        $this->assertSame(1, Cast::toInt(1));
+        $this->assertSame(1, Cast::toInt(1.1));
+        $this->assertSame(1, Cast::toInt(1.9));
+        $this->assertSame(0, Cast::toInt('0'));
         $this->assertSame(1, Cast::toInt('1'));
         $this->assertSame(1, Cast::toInt('1.1'));
+        $this->assertSame(1, Cast::toInt('1.9'));
+        $this->assertSame(0, Cast::toInt(false));
+        $this->assertSame(1, Cast::toInt(true));
+
+        $this->assertTypeError(fn () => Cast::toInt(''));
+        $this->assertTypeError(fn () => Cast::toInt('a'));
+        $this->assertTypeError(fn () => Cast::toInt('ok'));
+        $this->assertTypeError(fn () => Cast::toInt('true'));
+        $this->assertTypeError(fn () => Cast::toInt('null'));
+        $this->assertTypeError(fn () => Cast::toInt(null));
+        $this->assertTypeError(fn () => Cast::toInt([]));
+        $this->assertTypeError(fn () => Cast::toInt((object)[]));
+        $this->assertTypeError(fn () => Cast::toInt(PHP_INT_MAX + 1));
+        $this->assertTypeError(fn () => Cast::toInt(-9223372036854776833));
+    }
+
+    public function testToFloat()
+    {
+        $this->assertSame(0.0, Cast::toFloat(0));
+        $this->assertSame(1.0, Cast::toFloat(1));
+        $this->assertSame(1.1, Cast::toFloat(1.1));
+        $this->assertSame(1.9, Cast::toFloat(1.9));
+        $this->assertSame(0.0, Cast::toFloat('0'));
+        $this->assertSame(1.0, Cast::toFloat('1'));
+        $this->assertSame(1.1, Cast::toFloat('1.1'));
+        $this->assertSame(1.9, Cast::toFloat('1.9'));
+        $this->assertSame(0.0, Cast::toFloat(false));
+        $this->assertSame(1.0, Cast::toFloat(true));
+        $this->assertSame(PHP_INT_MAX + 1, Cast::toFloat(PHP_INT_MAX + 1));
+        $this->assertSame(-9223372036854776833, Cast::toFloat(-9223372036854776833));
+
+        $this->assertTypeError(fn () => Cast::toFloat(''));
+        $this->assertTypeError(fn () => Cast::toFloat('a'));
+        $this->assertTypeError(fn () => Cast::toFloat('ok'));
+        $this->assertTypeError(fn () => Cast::toFloat('true'));
+        $this->assertTypeError(fn () => Cast::toFloat('null'));
+        $this->assertTypeError(fn () => Cast::toFloat(null));
+        $this->assertTypeError(fn () => Cast::toFloat([]));
+        $this->assertTypeError(fn () => Cast::toFloat((object)[]));
+    }
+
+    public function testToString()
+    {
+        $this->assertSame('0', Cast::toString(0));
+        $this->assertSame('1', Cast::toString(1));
+        $this->assertSame('1.1', Cast::toString(1.1));
+        $this->assertSame('0', Cast::toString('0'));
+        $this->assertSame('1', Cast::toString('1'));
+        $this->assertSame('a', Cast::toString('a'));
+        $this->assertSame('1.1', Cast::toString('1.1'));
+        $this->assertSame('', Cast::toString(false));
+        $this->assertSame('1', Cast::toString(true));
+        $this->assertSame(
+            'stringable',
+            Cast::toString(new class () {public function __toString() {return 'stringable';}})
+        );
+
+        $this->assertTypeError(fn () => Cast::toString(null));
+        $this->assertTypeError(fn () => Cast::toString([]));
+        $this->assertTypeError(fn () => Cast::toString((object)[]));
+    }
+
+    public function testToBool()
+    {
+        $this->assertSame(false, Cast::toBool(0));
+        $this->assertSame(true, Cast::toBool(1));
+        $this->assertSame(true, Cast::toBool(1.1));
+        $this->assertSame(false, Cast::toBool('0'));
+        $this->assertSame(true, Cast::toBool('1'));
+        $this->assertSame(true, Cast::toBool('a'));
+        $this->assertSame(true, Cast::toBool('1.1'));
+        $this->assertSame(false, Cast::toBool(false));
+        $this->assertSame(true, Cast::toBool(true));
+        $this->assertSame(false, Cast::toBool(''));
+        $this->assertSame(true, Cast::toBool('a'));
+        $this->assertSame(true, Cast::toBool('ok'));
+        $this->assertSame(true, Cast::toBool('ng'));
+        $this->assertSame(true, Cast::toBool('false'));
+        $this->assertSame(true, Cast::toBool('true'));
+        $this->assertSame(true, Cast::toBool('null'));
+
+        $this->assertTypeError(fn () => Cast::toBool(null));
+        $this->assertTypeError(fn () => Cast::toBool([]));
+        $this->assertTypeError(fn () => Cast::toBool((object)[]));
+    }
+
+    private function assertTypeError(callable $func): void
+    {
         try {
-            Cast::toInt('a');
+            $func();
         } catch (Throwable $e) {
         }
         $this->assertTrue(isset($e));
