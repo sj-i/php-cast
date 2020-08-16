b5aedf967bed1ad5f51bf4b7ca5f9b8b9c91e9f0
sji <shinji.igarashi@gmail.com>
2020-08-16 23:15:56 +0900

    support running tests on PHP 7.0
    remove phpcs and psalm from require-dev and change to use tools in shivammathur/setup-php

--- tests/PhpCast/CastTest.php
+++ tests/PhpCast/CastTest.php
@@ -22,7 +22,7 @@ use TypeError;
 
 class CastTest extends TestCase
 {
-    public function testToInt(): void
+    public function testToInt()
     {
         $this->assertSame(0, Cast::toInt(0));
         $this->assertSame(1, Cast::toInt(1));
@@ -35,30 +35,52 @@ class CastTest extends TestCase
         $this->assertSame(0, Cast::toInt(false));
         $this->assertSame(1, Cast::toInt(true));
 
-        $this->assertTypeError(fn () => Cast::toInt(''));
-        $this->assertTypeError(fn () => Cast::toInt('a'));
-        $this->assertTypeError(fn () => Cast::toInt('ok'));
-        $this->assertTypeError(fn () => Cast::toInt('true'));
-        $this->assertTypeError(fn () => Cast::toInt('null'));
-        $this->assertTypeError(fn () => Cast::toInt(null));
-        $this->assertTypeError(fn () => Cast::toInt([]));
-        $this->assertTypeError(fn () => Cast::toInt((object)[]));
-        $this->assertTypeError(fn () => Cast::toInt(PHP_INT_MAX + 1));
-        $this->assertTypeError(fn () => Cast::toInt(-9223372036854776833));
+        $this->assertTypeError(function () {
+            return Cast::toInt('');
+        });
+        $this->assertTypeError(function () {
+            return Cast::toInt('a');
+        });
+        $this->assertTypeError(function () {
+            return Cast::toInt('ok');
+        });
+        $this->assertTypeError(function () {
+            return Cast::toInt('true');
+        });
+        $this->assertTypeError(function () {
+            return Cast::toInt('null');
+        });
+        $this->assertTypeError(function () {
+            return Cast::toInt(null);
+        });
+        $this->assertTypeError(function () {
+            return Cast::toInt([]);
+        });
+        $this->assertTypeError(function () {
+            return Cast::toInt((object)[]);
+        });
+        $this->assertTypeError(function () {
+            return Cast::toInt(PHP_INT_MAX + 1);
+        });
+        $this->assertTypeError(function () {
+            return Cast::toInt(-9223372036854776833);
+        });
 
         try {
             Cast::toInt('123abc');
         } catch (\Throwable $e) {
         }
-        $this->assertTrue(isset($e));
-        $this->assertInstanceOf(TypeError::class, $e);
-        $this->assertInstanceOf(Exception::class, $e->getPrevious());
-        $this->assertSame('A non well formed numeric value encountered', $e->getPrevious()->getMessage());
+        if (version_compare(PHP_VERSION, '7.1.0') >= 0) {
+            $this->assertTrue(isset($e));
+            $this->assertInstanceOf(TypeError::class, $e);
+            $this->assertInstanceOf(Exception::class, $e->getPrevious());
+            $this->assertSame('A non well formed numeric value encountered', $e->getPrevious()->getMessage());
+        }
         $result = @Cast::toInt('123abc');
         $this->assertSame(123, $result);
     }
 
-    public function testToFloat(): void
+    public function testToFloat()
     {
         $this->assertSame(0.0, Cast::toFloat(0));
         $this->assertSame(1.0, Cast::toFloat(1));
@@ -73,17 +95,33 @@ class CastTest extends TestCase
         $this->assertSame(PHP_INT_MAX + 1, Cast::toFloat(PHP_INT_MAX + 1));
         $this->assertSame(-9223372036854776833, Cast::toFloat(-9223372036854776833));
 
-        $this->assertTypeError(fn () => Cast::toFloat(''));
-        $this->assertTypeError(fn () => Cast::toFloat('a'));
-        $this->assertTypeError(fn () => Cast::toFloat('ok'));
-        $this->assertTypeError(fn () => Cast::toFloat('true'));
-        $this->assertTypeError(fn () => Cast::toFloat('null'));
-        $this->assertTypeError(fn () => Cast::toFloat(null));
-        $this->assertTypeError(fn () => Cast::toFloat([]));
-        $this->assertTypeError(fn () => Cast::toFloat((object)[]));
+        $this->assertTypeError(function () {
+            return Cast::toFloat('');
+        });
+        $this->assertTypeError(function () {
+            return Cast::toFloat('a');
+        });
+        $this->assertTypeError(function () {
+            return Cast::toFloat('ok');
+        });
+        $this->assertTypeError(function () {
+            return Cast::toFloat('true');
+        });
+        $this->assertTypeError(function () {
+            return Cast::toFloat('null');
+        });
+        $this->assertTypeError(function () {
+            return Cast::toFloat(null);
+        });
+        $this->assertTypeError(function () {
+            return Cast::toFloat([]);
+        });
+        $this->assertTypeError(function () {
+            return Cast::toFloat((object)[]);
+        });
     }
 
-    public function testToString(): void
+    public function testToString()
     {
         $this->assertSame('0', Cast::toString(0));
         $this->assertSame('1', Cast::toString(1));
@@ -106,12 +144,18 @@ class CastTest extends TestCase
             )
         );
 
-        $this->assertTypeError(fn () => Cast::toString(null));
-        $this->assertTypeError(fn () => Cast::toString([]));
-        $this->assertTypeError(fn () => Cast::toString((object)[]));
+        $this->assertTypeError(function () {
+            return Cast::toString(null);
+        });
+        $this->assertTypeError(function () {
+            return Cast::toString([]);
+        });
+        $this->assertTypeError(function () {
+            return Cast::toString((object)[]);
+        });
     }
 
-    public function testToBool(): void
+    public function testToBool()
     {
         $this->assertFalse(Cast::toBool(0));
         $this->assertTrue(Cast::toBool(1));
@@ -130,12 +174,18 @@ class CastTest extends TestCase
         $this->assertTrue(Cast::toBool('true'));
         $this->assertTrue(Cast::toBool('null'));
 
-        $this->assertTypeError(fn () => Cast::toBool(null));
-        $this->assertTypeError(fn () => Cast::toBool([]));
-        $this->assertTypeError(fn () => Cast::toBool((object)[]));
+        $this->assertTypeError(function () {
+            return Cast::toBool(null);
+        });
+        $this->assertTypeError(function () {
+            return Cast::toBool([]);
+        });
+        $this->assertTypeError(function () {
+            return Cast::toBool((object)[]);
+        });
     }
 
-    private function assertTypeError(callable $func): void
+    private function assertTypeError(callable $func)
     {
         try {
             $func();
